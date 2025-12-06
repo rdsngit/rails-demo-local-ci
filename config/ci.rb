7e0c5d704ce37f6b08e68ccd6d20ab16c04afc24
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-12-06 13:19:44 +0000

    update config/ci.rb with bin/rspec for tests step

--- config/ci.rb
+++ config/ci.rb
@@ -3,7 +3,7 @@
 CI.run do
   step "Setup", "bin/setup --skip-server"
 
-  step "Tests: Rails", "bin/rails test"
+  step "Tests: Rails", "bin/rspec"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
   # Optional: set a green GitHub commit status to unblock PR merge.
