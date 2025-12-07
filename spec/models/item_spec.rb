8da32ba33e569ffc0e598a3a01b2a5018fc7893a
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-12-07 14:58:34 +0000

    amend syntax to use is_expected.to in item_spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:description) }
-    it { should validate_presence_of(:price) }
-    it { should validate_numericality_of(:price) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_numericality_of(:price) }
   end
 end
