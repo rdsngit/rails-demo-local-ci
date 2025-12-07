ed6b6c637e03161a2b23a33d3cc47d2dbb5a392c
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-12-07 14:38:13 +0000

    add validations for :name, :description, and :price to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { precision: 6, scale: 2 }
 end
